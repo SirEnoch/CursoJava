9e898dcda2787c4fb9272b2e0b236bad409fc042
sammsouzaa <samu3ifpr@gmail.com>
2023-03-24 13:38:28 -0300

    attualizaçãoum

--- vetores/aarrays.java
+++ vetores/aarrays.java
@@ -9,22 +9,21 @@ public class aarrays {
         Scanner entrada = new Scanner(System.in);
 
         final int tam = 5;
-        char[] gabarito = {'a','a','d','b','c'};
+        char[] gabarito = { 'a', 'a', 'd', 'b', 'c' };
         char[] respostas = new char[tam];
-        int nota=0;
+        int nota = 0;
 
-        for(int i=0; i<tam; i++){
-            System.out.printf("Informe a %d respota: %n",i+1);
+        for (int i = 0; i < tam; i++) {
+            System.out.printf("Informe a %d respota: %n", i + 1);
             respostas[i] = entrada.nextLine().charAt(0);
         }
 
-        for(int i=0; i<tam; i++){
-            if (respostas[i] == gabarito[i]){
+        for (int i = 0; i < tam; i++) {
+            if (respostas[i] == gabarito[i]) {
                 nota++;
             }
         }
-
-        System.out.printf("%nNota do aluno: %d %n",nota);
+        System.out.printf("%nNota do aluno: %d %n", nota);
 
     }
 }
